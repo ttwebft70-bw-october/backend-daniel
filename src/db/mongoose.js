0f0724dcc8901b4509a7a7d292efdfdfdb9d2134
Daniel Antonio <danieltomantonio@gamil.com>
2020-10-20 06:48:08 -0700

    Testing mongodb connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,4 +6,10 @@ mongoose.connect(url, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
+})
+.then(res => {
+    console.log('Successfully connected to database server.');
+})
+.catch(err => {
+    console.log(err)
 });
