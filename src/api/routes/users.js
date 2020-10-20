f4b6f2bd05ceba673c7ce65c20b3f3c1b8d2cc6e
Daniel Antonio <danieltomantonio@gamil.com>
2020-10-20 08:13:28 -0700

    Testing mongodb connection

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -37,7 +37,7 @@ router.post('/register', async (req,res) => {
     }
     try {
         const newUser = new User(req.body);
-        await newUser.save();
+        const saveUser = await newUser.save();
         
         // const userObject = await User.findOne({ username: req.body.username }).lean();
         
@@ -45,7 +45,7 @@ router.post('/register', async (req,res) => {
         // const token = jwt.sign(userObject, secret, { expiresIn: '5hr' });
 
         // res.status(201).json({ token });
-        res.status(201).json(newUser);
+        res.status(201).json(saveUser);
     } catch(err) {
         // if(err.errors) {
         //     const errors = Object.keys(err.errors);
